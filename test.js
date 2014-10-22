01e1a7ed8c0a72b57f092b3f1c91a35d19a0f3eb
Deepak Vishwakarma <deepak.m.shrma@gmail.com>
2014-10-22 15:37:59 +0530

    console log removed..

--- test.js
+++ test.js
@@ -4,7 +4,7 @@
 var Behance = require("./index");
 var behance = new Behance({"client_id": "8kTASYLumlKK5wAcP02A2Rmc2NFKcfyj"})
 Behance.initOptions();
-behance.get('POST', Behance.APIS.USERS, {country: "pakistan"}, function (error, result) {
+behance.get('GET', Behance.APIS.USERS, {country: "pakistan"}, function (error, result) {
     if (error)
         console.log(error.message)
     else
