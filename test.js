cb403ded8e498b8b2a1b4ca8a2b6c4f9acb72d06
Deepak Vishwakarma <deepak.m.shrma@gmail.com>
2014-10-22 21:52:52 +0530

    added new apis entry in options.json, improve errror response and new apis enty in api names

--- test.js
+++ test.js
@@ -1,36 +1,12 @@
 /**
- * Created by deepak.m.shrma@gmail.com on 21/10/14.
- *
+ * Created by intelligrape on 21/10/14.
  */
-"use strict";
-var behance = require("./index"),
-    extend = require('extend');
-console.log(__dirname)
-
-behance.initOptions({
-    postProcess: function (options) {
-        var newapis = {
-            "users": {
-                "name": "users",
-                "url": "users",
-                "params": {
-                    "client_id": true,
-                    "q": false,
-                    "sort": false,
-                    "time": false,
-                    "field": false,
-                    "country": false,
-                    "state": false,
-                    "city": false,
-                    "page": false,
-                    "tags": false,
-                    "color_hex": false,
-                    "color_range": false
-                }
-            }
-        }
-        options = extend(true, {}, newapis, options);
-        return options;
-    }
-});
-console.log(behance._apiOptions)
+var Behance = require("./index")
+var behance = new Behance({"client_id": "xxxxxxxxxxxxxxxxxxxxxx"})
+Behance.initOptions();
+behance.get('GET', Behance.APIS.GET_WIP_REVISION_COMMENTS, {wip_id: "abc", revision_id: "1245", page: "2"}, function (error, result) {
+    if (error)
+        console.log(error)
+    else
+        console.log(result)
+})
