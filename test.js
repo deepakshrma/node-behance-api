c3045264969792f9f56dd1444b4b982b0b6bd052
deepakshrma <deepak.m.shrma@gmail.com>
2015-03-17 02:00:45 +0530

    added new way writing codes

--- test.js
+++ test.js
@@ -2,11 +2,14 @@
  * Created by intelligrape on 21/10/14.
  */
 var Behance = require("./index")
-var behance = new Behance({"client_id": "xxxxxxxxxxxxxxxxxxxxxx"})
+var behance = new Behance({"client_id": "xxxxxxxxxxxxxxxxxxxxxxx"})
 Behance.initOptions();
-behance.get('GET', Behance.APIS.GET_WIP_REVISION_COMMENTS, {wip_id: "abc", revision_id: "1245", page: "2"}, function (error, result) {
+behance.get({
+    api: Behance.APIS.GET_WIP_REVISION_COMMENTS,
+    params: {wip_id: "abc", revision_id: "1245", page: "2"}
+}, function (error, result) {
     if (error)
         console.log(error)
     else
         console.log(result)
-})
+});
